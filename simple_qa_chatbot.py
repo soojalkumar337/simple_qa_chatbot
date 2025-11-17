6e1b420a3b25981b595960731579e8b3aa177bd9
soojalkumar337 <kumarsoojal55@gmail.com>
2025-11-17 04:45:17 -0800

    Update simple_qa_chatbot.py

--- simple_qa_chatbot.py
+++ simple_qa_chatbot.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from langchain_groq import ChatGroq
+from langchain-groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -25,4 +25,5 @@ chain = prompt | llm | output_parser
 if input_text:
 
     st.write(chain.invoke({"question": input_text}))
+
 
