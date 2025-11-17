fbebe8ec2f17cdc0317820ac7ab12b68b8cb66ec
soojalkumar337 <kumarsoojal55@gmail.com>
2025-11-17 05:36:57 -0800

    Update simple_qa_chatbot.py

--- simple_qa_chatbot.py
+++ simple_qa_chatbot.py
@@ -1,11 +1,14 @@
 import os
 import streamlit as st
+from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+load_dotenv()
+#enviroment variables call
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 
-os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 # creating chatbot
 prompt = ChatPromptTemplate.from_messages([
@@ -24,11 +27,4 @@ output_parser = StrOutputParser()
 # chain
 chain = prompt | llm | output_parser
 if input_text:
-
     st.write(chain.invoke({"question": input_text}))
-
-
-
-
-
-
